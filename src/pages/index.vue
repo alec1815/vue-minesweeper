51d71992b45099a5843fcc9c02eeec40c01a4f86
wzhaofei <505139165@qq.com>
2022-04-25 18:20:53 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,24 @@
 import {toggleDev, isDev} from '~/composables'
 import { GamePlay } from '~/composables/logic';
 
-const play = new GamePlay(10,10,10)
+const play = new GamePlay(9,9,10)
 useStorage("vuesweeper-state",play.state)
 const state = computed(()=>play.board)
+
+function newGame(difficulty: 'easy' | 'medium' | 'hard'){
+  console.log(difficulty)
+  switch(difficulty){
+    case'easy':
+      play.reset(9,9,10)
+      break
+    case'medium':
+      play.reset(20,20,40)
+      break
+    case'hard':
+      play.reset(20,30,99)
+      break
+  }
+}
 
 watchEffect(()=>{
   play.checkGameState()
@@ -19,6 +34,12 @@ console.log("state",state)
 <template>
   <div>
     Minesweeper
+    <div flex="~ gap1" justify-center>
+      <button btn @click="play.reset()">New Game</button>
+      <button btn @click="newGame('easy')">Easy</button>
+      <button btn @click="newGame('medium')">Medium</button>
+      <button btn @click="newGame('hard')">Hard</button>
+    </div>
     <div p-5 w-full overflow-auto>
       <div v-for="row,y in state"
        :key="y"
@@ -37,7 +58,7 @@ console.log("state",state)
     <div>{{mineCount}}</div>
     <div flex="~ gap-1" justify-center >
       <button btn @click="toggleDev()">{{isDev ? 'DEV' : 'NORMAL'}}</button>
-      <button btn @click="play.reset()">RESET</button>
+      
     </div>
     <Confetti :passed="play.state.value.gameState === 'won'"></Confetti>
   </div>
